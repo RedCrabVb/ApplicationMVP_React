744c7f022d7a0767e79c4dd1c7482c221adc985c
Alex <a.borodai@neoflex.ru>
2022-04-26 21:46:38 +0300

    Todo list

--- App.js
+++ App.js
@@ -2,14 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {Navbar} from './src/Navbar'
 import {AddTodo} from './src/Addtodo'
-import React from "react";
+import {Todo} from './src/Todo'
+import React, {useState} from "react";
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const  addTodo = (title) => {
+    console.log(title + " addTodo")
+    setTodos(prev => [
+          ...prev, {
+          id: Date.now().toString(),
+          title: title
+        }]
+    )
+  }
+
   return (
     <View>
       <Navbar title='hello, todo app'></Navbar>
       <View style={styles.container}>
-        <AddTodo/>
+        <AddTodo onSubmit={addTodo}/>
+        <View>
+          {todos.map(todo => <Todo todo={todo}></Todo>)}
+        </View>
       </View>
     </View>
   );
--- src/Addtodo.js
+++ src/Addtodo.js
@@ -1,11 +1,25 @@
-import React from 'react'
-import {View, StyleSheet, TextInput, Button} from 'react-native'
+import React, {useState} from 'react'
+import {View, StyleSheet, TextInput, Button, Alert} from 'react-native'
 
-export const AddTodo = props => {
+export const AddTodo = ({ onSubmit }) => {
+    const [value, setValue] = useState('')
+
+    const pressHandler = () => {
+        if (value.trim()) {
+            onSubmit(value)
+            setValue('')
+        } else {
+            Alert.alert("Дело не может быть пустым")
+        }
+    }
+
     return (
         <View style={styles.block}>
-            <TextInput style={styles.input}/>
-            <Button title='Добавить'></Button>
+            <TextInput style={styles.input}
+                onChangeText={text => setValue(text)}
+                value={value}
+                placeholder="Введите название дела"/>
+            <Button title='Добавить' onPress={pressHandler}/>
         </View>
     )
 }
