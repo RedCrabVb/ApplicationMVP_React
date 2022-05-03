e5bf7da9db18c8f12208c11dfdcd7c63845d0268
Alex <a.borodai@neoflex.ru>
2022-05-03 16:49:50 +0300

    V 2.5.8 Fix UI login

--- navigation/screens/stack/LogIn.js
+++ navigation/screens/stack/LogIn.js
@@ -1,54 +1,90 @@
 import * as React from 'react'
-import {View, Vibration, Alert, AsyncStorage} from 'react-native'
+import {View, Vibration, Alert, AsyncStorage, Keyboard} from 'react-native'
 import {styles} from "../../../src/css/css"
 import {CustomInput} from "../../../src/component/CustomInput"
 import {useState} from "react"
 import {CustomButton} from "../../../src/component/CutomButton"
 import {USER} from "../../../src/utils/Storage"
 import {authorization} from "../../../src/utils/Api"
-import {settingsName, gameName, homeName, mailResetName} from '../../../src/utils/ScreenNames'
+import {settingsName, mailResetName} from '../../../src/utils/ScreenNames'
 
 export default function LogIn(props) {
     const [mail, setMail] = useState("")
     const [password, setPassword] = useState("")
+    const [errors, setErrors] = React.useState({})
 
-    const handlerPressAut = () => {
-        if (password.length < 4 || mail.length < 4) {
-            Alert.alert("Почта и пароль должны быть больше 5 символов")
-            Vibration.vibrate()
+    const validate = async () => {
+        Keyboard.dismiss()
+        let isValid = true
+        if (mail.length < 4) {
+            handleError('Логин должен быть длиннее 4 символов', 'email')
+            isValid = false
         } else {
-            AsyncStorage.getItem(USER).then(u => JSON.parse(u)).then(user => {
-                console.log(user);
-                const apiAut = `${authorization}?email=${mail}&password=${password}`;
-                const requestOptions = {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                };
-                fetch(apiAut, requestOptions)
-                    .then((response) => response.json())
-                    .then((data) => {
-                        console.log(data)
-                        if ('error' in data) {
-                            Alert.alert("Ошибка", "Ошибка при регистрации, попробуте:\n1) изменить данные \n2) сбросить кэш \n3) подключиться позже \n4) обратиться к администратору ")
-                            Vibration.vibrate()
-                        } else {
-                            AsyncStorage.setItem(USER, JSON.stringify(data))
-                            props.navigation.popToTop(settingsName)
-                        }
-                    })
-                    .catch((error) => Alert.alert(error))
-            });
+            handleError(null, 'email')
         }
+        if (password.length < 4) {
+            handleError('Пароль должен быть длиннее 4 символов', 'password')
+            isValid = false
+        } else {
+            handleError(null, 'password')
+        }
+        if (isValid) {
+            handlerAut()
+        }
+    }
+
+    const handleError = (error, input) => {
+        setErrors(prevState => ({...prevState, [input]: error}));
+    }
+
+    const handlerAut = () => {
+
+        AsyncStorage.getItem(USER).then(u => JSON.parse(u)).then(user => {
+            console.log(user);
+            const apiAut = `${authorization}?email=${mail}&password=${password}`;
+            const requestOptions = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+            };
+            fetch(apiAut, requestOptions)
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data)
+                    if ('error' in data) {
+                        Alert.alert("Ошибка", "Ошибка при авторизации, попробуте:\n1) изменить данные \n2) сбросить кэш \n3) подключиться позже \n4) обратиться к администратору ")
+                        Vibration.vibrate()
+                    } else {
+                        AsyncStorage.setItem(USER, JSON.stringify(data))
+                        props.navigation.popToTop(settingsName)
+                    }
+                })
+                .catch((error) => Alert.alert(error))
+        });
     }
 
 
     return (
         <View style={styles.container}>
-            <CustomInput value={mail} setValue={setMail} placeholder="Ваш логин"></CustomInput>
-            <CustomInput value={password} setValue={setPassword} secureTextEntry={true} placeholder="Ваш пароль" ></CustomInput>
-            <CustomButton onPress={handlerPressAut} text="Войти"></CustomButton>
-            <CustomButton text="Востоновить пароль" onPress={() => props.navigation.navigate(mailResetName)} style={{bgColor: "red"}}></CustomButton>
+            <CustomInput
+                value={mail}
+                onChangeText={setMail}
+                iconName={'people'}
+                error={errors.email}
+                placeholder="Ваш логин"/>
+            <CustomInput
+                value={password}
+                onChangeText={setPassword}
+                iconName={'lock-closed'}
+                password={true}
+                error={errors.password}
+                placeholder="Ваш пароль"/>
+            <CustomButton
+                onPress={validate}
+                text="Войти"/>
+            <CustomButton
+                text="Востоновить пароль"
+                onPress={() => props.navigation.navigate(mailResetName)} style={{bgColor: "red"}}/>
 
         </View>
-    );
+    )
 }
--- src/component/CustomInput.js
+++ src/component/CustomInput.js
@@ -1,37 +1,110 @@
 import React from 'react'
-import {TextInput, View, StyleSheet} from 'react-native'
+import {TextInput, View, Text, StyleSheet} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
-export const CustomInput = ({value,
-                                setValue,
-                                placeholder,
-                                secureTextEntry = false}) => {
+// export const CustomInput = ({value,
+//                                 setValue,
+//                                 placeholder,
+//                                 secureTextEntry = false}) => {
+//
+//     return (
+//         <View style={styles.container}>
+//             <TextInput
+//                 value={value}
+//                 onChangeText={setValue}
+//                 placeholder={placeholder}
+//                 style={styles.input}
+//                 secureTextEntry={secureTextEntry}
+//             >
+//
+//             </TextInput>
+//         </View>
+//     );
+// };
+//
+// const styles = StyleSheet.create({
+//     container : {
+//         backgroundColor: 'white',
+//         width: '100%',
+//
+//         borderColor: '#e8e8e8',
+//         borderWidth: 1,
+//         borderRadius: 5,
+//
+//         paddingHorizontal: 10,
+//         marginVertical: 5
+//     },
+//     input: {}
+// });
 
+export const CustomInput = ({
+                   label,
+                   iconName = 'home',
+                   error,
+                   password = false,
+                   onFocus = () => {},
+                   ...props
+               }) => {
+    const [hidePassword, setHidePassword] = React.useState(password);
+    const [isFocused, setIsFocused] = React.useState(false);
     return (
-        <View style={styles.container}>
-            <TextInput
-                value={value}
-                onChangeText={setValue}
-                placeholder={placeholder}
-                style={styles.input}
-                secureTextEntry={secureTextEntry}
-            >
-
-            </TextInput>
+        <View style={{marginBottom: 20}}>
+            <Text style={style.label}>{label}</Text>
+            <View
+                style={[
+                    style.inputContainer,
+                    {
+                        borderColor: error
+                            ? 'red'
+                            : isFocused
+                                ? '#7978B5'
+                                : 'white',
+                        alignItems: 'center',
+                    },
+                ]}>
+                <Icon
+                    name={iconName}
+                    style={{color: '#7978B5', fontSize: 22, marginRight: 10}}
+                />
+                <TextInput
+                    autoCorrect={false}
+                    onFocus={() => {
+                        onFocus();
+                        setIsFocused(true);
+                    }}
+                    onBlur={() => setIsFocused(false)}
+                    secureTextEntry={hidePassword}
+                    style={{color: '#7978B5', flex: 1}}
+                    {...props}
+                />
+                {password && (
+                    <Icon
+                        onPress={() => setHidePassword(!hidePassword)}
+                        name={hidePassword ? 'eye-outline' : 'eye-off-outline'}
+                        style={{color: '#7978B5', fontSize: 22}}
+                    />
+                )}
+            </View>
+            {error && (
+                <Text style={{marginTop: 7, color: 'red', fontSize: 12}}>
+                    {error}
+                </Text>
+            )}
         </View>
     );
 };
 
-const styles = StyleSheet.create({
-    container : {
-        backgroundColor: 'white',
-        width: '100%',
-
-        borderColor: '#e8e8e8',
-        borderWidth: 1,
-        borderRadius: 5,
-
-        paddingHorizontal: 10,
-        marginVertical: 5
+const style = StyleSheet.create({
+    label: {
+        marginVertical: 5,
+        fontSize: 14,
+        color: 'grey',
     },
-    input: {}
+    inputContainer: {
+        height: 55,
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        paddingHorizontal: 15,
+        borderWidth: 0.5,
+    },
 });
